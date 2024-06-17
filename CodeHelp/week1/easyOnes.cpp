78e0dcd18dcdf2f23f1fd5498815460f4d51fb3f
Ashish <ashishsinghcshp@gmail.com>
2024-06-18 00:16:21 +0530

    multiply 2 no

---
+++ CodeHelp/week1/easyOnes.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter no. to multiply"<<endl;
+    cin >>a>>b;
+    cout<<"multiply of a and b is -  "<<a*b;
+
+}
+//q1
+
