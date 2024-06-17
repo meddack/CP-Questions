66f179d8ae8f6e3edd6e65bb4c015b83628b4b25
Ashish <ashishsinghcshp@gmail.com>
2024-06-18 00:33:05 +0530

    factorial

--- CodeHelp/week1/easyOnes.cpp
+++ CodeHelp/week1/easyOnes.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    int a,b;
-    cout<<"enter no. to multiply"<<endl;
-    cin >>a>>b;
-    cout<<"multiply of a and b is -  "<<a*b;
+    // MULTIPLE 2 NO BY TAKING I/P
+    // int a,b;
+    // cout<<"enter no. to multiply"<<endl;
+    // cin >>a>>b;
+    // cout<<"multiply of a and b is -  "<<a*b;
+
+    //perimeter of traangle 
+
+    
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<"perimeter of triangle -- "<<a+b+c;
+
+
+
 
 }
-//q1
+
 
